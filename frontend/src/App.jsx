93b43e0f23e6213e05d745952de4bb64f27efefb
Daris02 <hei.raymond.2@gmail.com>
2023-06-20 11:50:43 +0300

    feat: get data from server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Routes, Route} from 'react-router-dom'
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home/Home";
 import List from "./pages/List/List";
 import Form from "./pages/Form/Form";
 
+const baseURL = 'http://localhost:5000';
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${baseURL}/users`)
+      .then((response) => setData(response.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <header>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/list" element={<List />} />
+          <Route path="/list" element={<List data={data} />} />
           <Route path="/form" element={<Form />} />
         </Routes>
-      </header>
+      </header>y
     </>
   );
 }
--- frontend/src/pages/List/List.jsx
+++ frontend/src/pages/List/List.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-export default function List() {
+export default function List({ data }) {
+  
   return (
     <>
         <main className='container d-flex justify-content-center'>
             <h2>List of All User</h2>
+            {
+              data.map(d => {
+                <p key={d.id}>{d.name}</p>
+              })
+            }
         </main>
     </>
   );
