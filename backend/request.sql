edb4fe82a30b72542469726d3bd06ff832093e77
Daris02 <hei.raymond.2@gmail.com>
2023-07-22 21:35:36 +0300

    fix: change column in database

--- backend/request.sql
+++ backend/request.sql
@@ -1,13 +1,13 @@
--- Active: 1686752343750@@localhost@5432@users
+-- Active: 1689793390378@@localhost@5432@users
 CREATE TABLE "user" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(255),
-    dob DATE
+    password VARCHAR(255)
 );
 
-INSERT INTO "user" (name, email, dob)
+INSERT INTO "user" (name, email, password)
 VALUES
-('Alice', 'alice@gmail.com', '1997-01-11'),
-('Bob', 'bob@gmail.com', '2000-02-22'),
-('Clark', 'clark@gmail.com', '2003-03-03');
+('Alice', 'alice@gmail.com', 'alice123'),
+('Bob', 'bob@gmail.com', 'bob456'),
+('Clark', 'clark@gmail.com', 'clark789');
