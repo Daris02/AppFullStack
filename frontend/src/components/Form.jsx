f9ea2788a429a6ef65b6be8c87bfb17c0daaebe7
Daris02 <hei.raymond.2@gmail.com>
2023-08-20 10:52:19 +0300

    feat: add form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
+import { useForm } from "react-hook-form";
+import ApiUser from '../../api/api.user';
 
 export default function Form() {
+  const { register, handleSubmit, watch, formState: {errors, isValid } } = useForm({});
+
+  const onSubmit = (data) => {
+    ApiUser.addUser(data)
+  }
+
   return (
     <>
-        <>
-            <form>
-              
-            </form>
-        </>
+        <main className="container d-flex flex-column align-items-center gap-5">
+        <h2>Form for Add New User</h2>
+        <form className="row g-3 w-25" onSubmit={handleSubmit(onSubmit)}>
+          <input type="hidden" defaultValue={Date.now()} {...register("id")} />
+          <input
+            className="form-control"
+            type="text"
+            name="name"
+            placeholder="Name"
+            {...register("name", {required: true})}
+          />
+          {errors.name && <span className="text-danger">Name required /!\</span>}
+          <input
+            className="form-control"
+            type="email"
+            name="email"
+            placeholder="Email"
+            {...register("email")}
+          />
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            placeholder="Password"
+            {...register("password")}
+          />
+          <input type="submit" disabled={!isValid} value="Add" className="form-control btn btn-primary" />
+        </form>
+      </main>
     </>
   )
 }
