dcec9872a0b762b08c993bf9c5ca8945053cbf2d
Daris02 <hei.raymond.2@gmail.com>
2023-09-02 21:18:22 +0300

    fix(front): optimise list of user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { Routes, Route} from 'react-router-dom'
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home/Home";
 import List from "./pages/List/List";
 import Add from "./pages/Add/Add";
-import { getUsers } from "./api/api.user";
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    async function getUsersLoad() {
-      const allUsers = await getUsers();
-      setUsers(allUsers);
-    }
-
-    getUsersLoad();
-    
-  }, []);
-
   return (
     <>
       <header>
@@ -27,7 +12,7 @@ function App() {
       </header>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/list" element={<List data={users} />} />
+          <Route path="/list" element={<List />} />
           <Route path="/add" element={<Add />} />
         </Routes>
     </>
--- frontend/src/pages/List/List.jsx
+++ frontend/src/pages/List/List.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getUsers } from "../../api/api.user";
 
-export default function List({ data }) {
+export default function List() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsersLoad() {
+      const allUsers = await getUsers();
+      setUsers(allUsers);
+    }
+
+    getUsersLoad();
+    
+  }, []);
+
   return (
     <>
       <main className="container d-flex flex-column align-items-center">
         <h2>List of All User</h2>
         <ul>
-          {data ? data.map((user) => <li key={user.id}>{user.name}</li>) : <p>Loading...</p>}
+          {users ? users.map((user) => <li key={user.id}>{user.name}</li>) : <p>Loading...</p>}
         </ul>
       </main>
     </>
